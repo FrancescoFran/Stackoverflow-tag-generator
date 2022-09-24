3373f6ca45db9ab2a6b788ba8803d909fd229b05
francesco_loiudice <francesco.loiudice@hotmail.it>
2022-09-24 16:31:27 +0200

    update app.py and website.html with language check

--- app.py
+++ app.py
@@ -25,30 +25,32 @@ def main():
         x = txt_ppc.lang_check(text)
 
         results = {}
-        # clean text
-        x = txt_ppc.cleaner(x)
+        if x != 'Not english':
+            # clean text
+            x = txt_ppc.cleaner(x)
 
-        # vectorize
-        x_tfidf = vect.transform([x])
+            # vectorize
+            x_tfidf = vect.transform([x])
 
-        # predict
-        predict = model.predict(x_tfidf)
-        predict_prob = model.predict_proba(x_tfidf)
-        tags_predict = multilabel_bin.inverse_transform(predict)
+            # predict
+            predict = model.predict(x_tfidf)
+            predict_prob = model.predict_proba(x_tfidf)
+            tags_predict = multilabel_bin.inverse_transform(predict)
 
-        a = []
-        for j in range(0, 50):
-            a.append(round(predict_prob[0, j] * 100, 1))
+            a = []
+            for j in range(0, 50):
+                a.append(round(predict_prob[0, j] * 100, 1))
 
-        df = pd.DataFrame(columns=['tags', 'probs'])
-        df['tags'] = multilabel_bin.classes_
-        df['probs'] = a
-        df = df.sort_values('probs', ascending=False).head(10)
+            df = pd.DataFrame(columns=['tags', 'probs'])
+            df['tags'] = multilabel_bin.classes_
+            df['probs'] = a
+            df = df.sort_values('probs', ascending=False).head(10)
 
-        results['Tags'] = tags_predict
-        results['Probabilities'] = df.set_index('tags')['probs'].to_dict()
-
-
+            results['Tags'] = tags_predict
+            results['Probabilities'] = df.set_index('tags')['probs'].to_dict()
+            results['lang_check'] = ''
+        else:
+            results['lang_check'] = 'Please reformulate your question in english'
     else:
         results = ""
 
